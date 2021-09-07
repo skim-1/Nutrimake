5fd2c55036b50019e1d2e50bad15fc506aa55abc
sunny <sunny2004.kim@gmail.com>
2021-09-07 00:09:57 -0500

    '9/7'

--- components/task.js
+++ components/task.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-import Checkbox from './checkbox';
-
 const TaskItem = () => {
 
   return (
     <View style={styles.item}>
       <View style={styles.itemLeft}>
         <View style={{paddingRight: 15}}>
-          <Checkbox/>
+          <TouchableOpacity style={styles.dButton} />
         </View>
         <Text style={styles.taskText}>asdff</Text>
       </View>
@@ -19,25 +17,30 @@ const TaskItem = () => {
 }
 
 const styles = StyleSheet.create({
-item: {
-  padding: 15,
-  backgroundColor: '#fff',
-  borderRadius: 10,
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  flexDirection: 'row',
-  marginBottom: 15,
-  height: 55,
-  width: 350,
-},
-itemLeft: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  flexWrap: 'wrap'
-},
-taskText: {
-  maxWidth: '80%',
-},
+  item: {
+    padding: 15,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 15,
+    height: 55,
+    width: 350,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  taskText: {
+    maxWidth: '80%',
+  },
+  dButton: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#4EA3B0"
+  }
 });
 
 export default TaskItem;
