e71e21f246bdb53407580fd4abe6c094c00c5a0a
bigman69 <sunny2004.kim@gmail.com>
2021-10-09 09:51:36 -0500

    didnt work

---
+++ components/scanner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, StatusBar, TouchableOpacity } from 'react-native';
+
+export default function Scanner({navigation}) {
+  return (
+    <Text>asdf;aklsdfa;dslkfj</Text>
+  )
+}
